088444e5f4396c7b75d7d641902cdc05cac3fc13
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-10-27 13:21:41 +0200

    fix

--- run-consumer.py
+++ run-consumer.py
@@ -43,9 +43,9 @@ PATHS = {
     "remoteConsumer-remoteMonica": {
         "path-to-data-dir": "data/",
         "path-to-final-output-dir": "/out/out/",
-        "path-to-output-dir": "/scratch/klima_konform/out/out/",
+        "path-to-output-dir": "/scratch/rpm/klima_konform/out/out/",
         #"path-to-csv-output-dir": "/out/csv-out/",
-        "path-to-csv-output-dir": "/scratch/klima_konform/out/csv-out/"
+        "path-to-csv-output-dir": "/scratch/rpm/klima_konform/out/csv-out/"
     }
 }
 TEMPLATE_SOIL_PATH = "{local_path_to_data_dir}germany/buek200_1000_25832_etrs89-utm32n.asc"
